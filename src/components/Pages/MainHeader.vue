326719b102b6f567753b6c9022e749fa487eb078
Fabio Elisio <elisio@polisystem.com.br>
2017-08-23 21:13:20 -0300

    /release-1: create Component SVG CidadeSsa

--- src/components/Pages/MainHeader.vue
+++ src/components/Pages/MainHeader.vue
@@ -1,21 +1,29 @@
 <template>
-  <div>
-    <div class="card">
-      <div class="card-body">
-        <blockquote class="blockquote mb-0">
-          <p>Aqui fica o svg da Principal</p>
-        </blockquote>
-      </div>
-    </div>
+  <div class="main-header">
+    <CidadeSsa />
   </div>
 </template>
 
 <script>
+  import CidadeSsa from '../Commom/Svg/CidadeSsa'
+
   export default {
     data () {
       return {
         msg: 'Pagina Principal'
       }
+    },
+    components: {
+      CidadeSsa
     }
   }
 </script>
+
+<style lang="stylus" scoped>
+  $border = solid 2px #fff
+  $padding = 0 4rem
+  .main-header
+    border-left $border
+    border-right $border
+    padding: $padding
+</style>
