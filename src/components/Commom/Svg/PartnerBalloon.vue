e8288cdcef8b3fb8ddf6e215bb28ba4284cceb63
Jader Gomes <jjadergomes@gmail.com>
2017-08-30 01:57:45 -0300

    Inserte svg. Separate svg file

---
+++ src/components/Commom/Svg/PartnerBalloon.vue
@@ -0,0 +1,22 @@
+<template>
+	<svg class="partner-balloon" viewBox="0 0 159 64" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+	    <defs></defs>
+	    <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
+	        <g id="parceiros" transform="translate(1.000000, -104.000000)" stroke="#FFFFFF" stroke-width="2">
+	            <path d="M139.6,105.5 L17.4,105.5 C8.2,105.5 0.8,112.9 0.8,122.1 L0.8,139.8 C0.8,149 8.2,156.4 17.4,156.4 L68.8,156.4 L78.5,166.1 L88.2,156.4 L139.6,156.4 C148.8,156.4 156.2,149 156.2,139.8 L156.2,122.1 C156.2,113 148.7,105.5 139.6,105.5 Z" id="XMLID_987_"></path>
+	        </g>
+	    </g>
+	</svg>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style type="text/css">
+	.partner-balloon {
+		width: 400px;
+		height: 140px;
+	}
+</style>
