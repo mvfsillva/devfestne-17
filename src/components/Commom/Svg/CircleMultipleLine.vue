5c71bb621e45bd000d05054af742f9b9dd00c6c0
Fabio Elisio <elisio@polisystem.com.br>
2017-08-29 21:32:10 -0300

    /release-1: create mini clouds

--- src/components/Commom/Svg/CircleMultipleLine.vue
+++ src/components/Commom/Svg/CircleMultipleLine.vue
@@ -1,6 +1,24 @@
 <template>
   <svg version="1.1" class="svg-circle" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
 	 viewBox="-167 0 910 200" style="enable-background:new 0 0 229.5 66;" xml:space="preserve">
+    <svg version="1.1" id="cloud-left" x="0px" y="0px" viewBox="20 0 900 70" style="enable-background:new 0 0 229.5 84;" xml:space="preserve">
+      <g>
+      <g>
+        <path class="mini-cloud-left" d="M105.7,49H138c3,0,5.4-2.4,5.4-5.4c0-3-2.4-5.4-5.4-5.4c-0.6,0-1.1,0.1-1.7,0.3c0.2-0.7,0.3-1.4,0.3-2.1
+          c0-4.4-3.6-8-8-8c-2.3,0-4.4,1-5.8,2.5c-0.7-6-5.8-10.6-11.9-10.6c-6.4,0-11.6,5-12,11.3c-1.2-1.3-2.9-2.1-4.9-2.1
+          c-3.3,0-6.1,2.4-6.7,5.6c-1-0.9-2.3-1.5-3.7-1.5c-0.9,0-1.7,0.2-2.4,0.6c0.1-0.4,0.1-0.9,0.1-1.3c0-4.6-3.7-8.3-8.3-8.3
+          c-2.7,0-5.1,1.3-6.6,3.3c-1-2.9-3.8-5-7.1-5c-4.2,0-7.5,3.4-7.5,7.5c0,0.6,0.1,1.2,0.2,1.8c-0.4-0.1-0.8-0.1-1.2-0.1
+          c-3,0-5.4,2.4-5.4,5.4c0,3,2.4,5.4,5.4,5.4H33.6"/>
+        <path class="mini-cloud-left" d="M121,47"/>
+        <line class="mini-cloud-left" x1="106.7" y1="61.7" x2="85.4" y2="61.7"/>
+        <line class="mini-cloud-left" x1="82.5" y1="61.7" x2="79.3" y2="61.7"/>
+        <polyline class="mini-cloud-left" points="75.6,61.7 74.7,61.7 67.4,61.7 		"/>
+      </g>
+      <line class="mini-cloud-left" x1="193" y1="37.5" x2="171.6" y2="37.5"/>
+      <line class="mini-cloud-left" x1="168.7" y1="37.5" x2="165.5" y2="37.5"/>
+      <polyline class="mini-cloud-left" points="162.5,37.5 161.7,37.5 154.4,37.5 	"/>
+    </g>
+    </svg>
     <g transform="rotate(90 150 160)">
       <line class="circle-lines" x1="10" y1="22" x2="20" y2="22"/>
       <line class="circle-lines" x1="50" y1="22" x2="27" y2="22"/>
@@ -9,6 +27,19 @@
       <line class="circle-lines" x1="99" y1="22" x2="148" y2="22"/>
       <line class="circle-lines" x1="156" y1="22" x2="190" y2="22"/>
     </g>
+    <svg version="1.1" id="cloud-right" x="0px" y="0px" viewBox="-40 10 300 224" style="enable-background:new 0 0 229.5 84;" xml:space="preserve">
+      <g>
+      <path class="mini-cloud-right" d="M158.3,36.2c0.8-4.6-2.4-9-7-9.8c-2.4-0.4-4.8,0.3-6.5,1.7c-0.6-4.1-3.3-7.8-7.4-9.5
+        c-6.3-2.7-13.6,0.2-16.2,6.5c-0.9,2.1-1.2,4.4-0.9,6.6c-1.1-0.9-2.4-1.5-3.9-1.7c-4.6-0.8-9,2.4-9.8,7c-0.1,0.8-0.1,1.5-0.1,2.3
+        c-1.1-0.7-2.4-1.1-3.8-1.1c-3.7,0-6.7,2.8-7.1,6.4c-1.3-1.4-3.1-2.2-5.1-2.2c-3.9,0-7.1,3.2-7.1,7.1c0,3.9,3.2,7.1,7.1,7.1H71.6"/>
+      <line class="mini-cloud-right" x1="145.3" y1="54.2" x2="170.4" y2="54.2"/>
+      <line class="mini-cloud-right" x1="172.1" y1="54.2" x2="175.9" y2="54.2"/>
+      <polyline class="mini-cloud-right" points="181.1,54.2 182.1,54.2 190.7,54.2 	"/>
+      <line class="mini-cloud-right" x1="139.8" y1="66.4" x2="61.5" y2="66.4"/>
+      <line class="mini-cloud-right" x1="43.3" y1="66.4" x2="34.8" y2="66.4"/>
+      <line class="mini-cloud-right" x1="59" y1="66.4" x2="50.5" y2="66.4"/>
+    </g>
+    </svg>
   </svg>
 </template>
 
@@ -25,6 +56,18 @@
   .circle-lines{
     fill:none;
     stroke: #feffff;
+    stroke-width:2;
+    stroke-miterlimit:10;
+  }
+  .mini-cloud-left{
+    fill:none;
+    stroke:#444545;
+    stroke-width:2;
+    stroke-miterlimit:10;
+  }
+  .mini-cloud-right{
+    fill:none;
+    stroke:#444545;
     stroke-width:2;
     stroke-miterlimit:10;
   }
