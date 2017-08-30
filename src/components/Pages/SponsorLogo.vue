707aae6583c3ccd4605206d755f3eb86693204ee
Jader Gomes <jjadergomes@gmail.com>
2017-08-30 01:58:38 -0300

    Create new components to sponsor and ticket block screen

---
+++ src/components/Pages/SponsorLogo.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+		  <circle-divisor/>
+      <div class="aa">
+        <h2 class="h2-heading text-success">Patrocinadores</h2>
+        <partner-balloon/>
+      </div>
+  </div>
+</template>
+
+<script>
+import CircleDivisor from '../Commom/Svg/CircleDivisor'
+import PartnerBalloon from '../Commom/Svg/PartnerBalloon'
+export default {
+  components: {CircleDivisor, PartnerBalloon}
+}
+</script>
+
+<style lang="stylus" scoped>
+</style>
---
+++ src/components/Pages/TicketBlock.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="ticket-block">
+		<div class="container">
+	  	<mini-clouds-section/>
+	  </div>
+  </div>
+</template>
+
+<script>
+import MiniCloudsSection from '../Template/MiniCloudsSection'
+export default {
+  components: {MiniCloudsSection}
+}
+</script>
+
+<style lang="stylus" scoped>
+.ticket-block
+		margin-top 2em
+</style>
