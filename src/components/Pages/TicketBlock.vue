68680214650658b751c6d002f40dfbf60dfc6301
Fabio Elisio <elisio@polisystem.com.br>
2017-08-30 22:05:45 -0300

    /release-11: create block wave / sum/ farol

--- src/components/Pages/TicketBlock.vue
+++ src/components/Pages/TicketBlock.vue
@@ -5,6 +5,15 @@
         <img :src="ticket" alt="Comprar" >
       </a>
 	  	<mini-clouds-section/>
+      <div class="sum">
+        <img :src="sum" alt="">
+      </div>
+      <div class="farol-barra">
+        <img :src="farol" alt="">
+      </div>
+      <div class="wave-yellow">
+        <img :src="wave" alt="">
+      </div>
 	  </div>
   </div>
 </template>
@@ -12,10 +21,17 @@
 <script>
 import MiniCloudsSection from '../Template/MiniCloudsSection'
 import Ticket from '../../assets/img/ticket.png'
+import FarolBarra from '../../assets/img/svg/farol_da_barra.svg'
+import WaveYellow from '../../assets/img/svg/ondas_amarelas.svg'
+import Sum from '../../assets/img/svg/sol.svg'
+
 export default {
   data () {
     return {
-      ticket: Ticket
+      ticket: Ticket,
+      sum: Sum,
+      farol: FarolBarra,
+      wave: WaveYellow
     }
   },
   components: {MiniCloudsSection}
@@ -35,5 +51,18 @@ export default {
     margin-top: 14rem
     img
       background-color #000
+  .sum
+    width: 5rem
+    position: absolute
+    right: 12rem
+    margin-top: 8rem
+  .farol-barra
+    width: 10rem
+    position: absolute
+    right: 7rem
+    margin-top: 12rem
+  .wave-yellow
+    position: relative
+    top: 18rem
 
 </style>
