87e8b200ab77495d942457b150318c406a3846a7
Fabio Elisio <elisio@polisystem.com.br>
2017-08-30 21:32:34 -0300

    /release-11: create link

--- src/components/Pages/TicketBlock.vue
+++ src/components/Pages/TicketBlock.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="ticket-block">
 		<div class="container">
+      <a href="https://loja.tampa.works/produto/devfest-nordeste-2017/" target="_blank" class="link-ticket">
+        <img :src="ticket" alt="Comprar" >
+      </a>
 	  	<mini-clouds-section/>
 	  </div>
   </div>
@@ -8,12 +11,29 @@
 
 <script>
 import MiniCloudsSection from '../Template/MiniCloudsSection'
+import Ticket from '../../assets/img/ticket.png'
 export default {
+  data () {
+    return {
+      ticket: Ticket
+    }
+  },
   components: {MiniCloudsSection}
 }
 </script>
 
 <style lang="stylus" scoped>
 .ticket-block
-		margin-top 2em
+  margin-top 2em
+  .link-ticket
+    position: absolute
+    left: 0
+    right: 0
+    display: flex
+    align-items: center
+    justify-content: center
+    margin-top: 14rem
+    img
+      background-color #000
+
 </style>
