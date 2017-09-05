a249d8b5ab2704db6bea70d9d802855729084b0a
Jader Gomes <jjadergomes@gmail.com>
2017-09-05 18:49:50 -0300

    Align text on center and add color one by one

--- src/components/Pages/StatisticsBlock.vue
+++ src/components/Pages/StatisticsBlock.vue
@@ -1,22 +1,22 @@
 <template>
 	<div class="container">
 		<div class="content">
-			<div class="row">
+			<div class="row stats">
 				<div class="col border border-white">
 					<h1 class="display-2">600</h1>
-					<p class="text-warning text-uppercase">Participantes</p>
+					<p class="stats-text red">Participantes</p>
 				</div>
 				<div class="col border border-white">
 					<h1 class="display-2">01</h1>
-					<p class="text-success text-uppercase">Dias de evento</p>
+					<p class="stats-text blue">Dia de evento</p>
 				</div>
 				<div class="col border border-white">
-					<h1 class="display-2">20</h1>
-					<p class="text-danger text-uppercase">Palestras</p>
+					<h1 class="display-2">22</h1>
+					<p class="stats-text green">Palestras</p>
 				</div>
 				<div class="col border border-white">
 					<h1 class="display-2">02</h1>
-					<p class="text-info text-uppercase">Trilhas</p>
+					<p class="stats-text yellow">Trilhas</p>
 				</div>
 			</div>
 		</div>
@@ -29,4 +29,23 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
+.content
+	padding 40px 0 10px
+.stats
+	max-width 980px
+	margin 0 auto
+	color #fff
+	text-align center
+	overflow hidden
+	padding-bottom 30px
+	.stats-text
+		text-transform uppercase
+		&.red
+			color #df4b3a
+		&.blue
+			color #4887f7
+		&.green
+			color #00a158
+		&.yellow
+			color #ffc300
 </style>
