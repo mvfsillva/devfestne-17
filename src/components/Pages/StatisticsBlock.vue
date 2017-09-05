016a2a99409ce58539de86df069815bce491fc19
Jader Gomes <jjadergomes@gmail.com>
2017-09-04 21:41:36 -0300

    Create component and add four block in container

---
+++ src/components/Pages/StatisticsBlock.vue
@@ -0,0 +1,32 @@
+<template>
+	<div class="container">
+		<div class="content">
+			<div class="row">
+				<div class="col border border-white">
+					<h1 class="display-2">600</h1>
+					<p class="text-warning text-uppercase">Participantes</p>
+				</div>
+				<div class="col border border-white">
+					<h1 class="display-2">01</h1>
+					<p class="text-success text-uppercase">Dias de evento</p>
+				</div>
+				<div class="col border border-white">
+					<h1 class="display-2">20</h1>
+					<p class="text-danger text-uppercase">Palestras</p>
+				</div>
+				<div class="col border border-white">
+					<h1 class="display-2">02</h1>
+					<p class="text-info text-uppercase">Trilhas</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="stylus" scoped>
+</style>
