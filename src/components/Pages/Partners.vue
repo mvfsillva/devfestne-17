765671b01dcbd98124d6b04a6cc500d6b0358d80
Jefferson Moura <jefinho.moura14@gmail.com>
2017-09-07 00:00:49 -0300

    Partners and sponsors section

--- src/components/Pages/Partners.vue
+++ src/components/Pages/Partners.vue
@@ -1,16 +1,12 @@
 <template>
 	<section class="container section-partners">
-		<header class="header">
+		<div class="header">
 			<h4 class="partners-title">Apoio</h4>
-		</header>
+		</div>
 		<!-- Partners logo -->
 		<div class="row">
-			<div class="col-2 mx-auto">
-				<div class="card">
-					<div class="card-body">
-						 <img class="card-img-top" :src="NineNine" alt="">
-					</div>
-				</div>
+			<div class="partner">
+				<img class="card-img-top" :src="NineNine" alt="">
 			</div>
 		</div>
 	</section>
@@ -29,9 +25,17 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
-@import '~style/base/variables'
-.section-partners
-  padding 20px 0 20px
-	.card
-		background transparent
+	.header
+		padding-bottom 40px
+
+	.section-partners
+		.row
+			display flex
+			justify-content space-around
+			.partner
+				width 300px
+				display block
+
+				img
+					width 100%
 </style>
