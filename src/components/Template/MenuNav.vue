eecc9c653eff76d3b414b102fb508f9e6aedde82
Jader Gomes <jjadergomes@gmail.com>
2017-09-07 17:15:46 -0300

    Add links and button to buy ticket

--- src/components/Template/MenuNav.vue
+++ src/components/Template/MenuNav.vue
@@ -1,20 +1,27 @@
 <template>
-  <div class="menu-nav">
-    <ul class="nav justify-content-end">
-      <li class="nav-item">
-        <a class="nav-link active" v-smooth-scroll="{ duration: 2000 }" href="#home">Home</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" v-smooth-scroll="{ duration: 2000 }" href="#event">O evento</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" v-smooth-scroll="{ duration: 2000 }" href="#where">Onde?</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" v-smooth-scroll="{ duration: 2000 }" href="#tickets">Comprar Ingresso</a>
-      </li>
-    </ul>
-  </div>
+  <nav class="menu-nav navbar fixed-top navbar-expand-lg">
+    <div class="container">
+      <div class="collapse navbar-collapse">
+        <ul class="nav navbar-nav mr-auto mt-2 mt-lg-0">
+          <li class="nav-item active">
+            <a class="nav-link active" v-smooth-scroll="{ duration: 2000 }" href="#home">Home</a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" v-smooth-scroll="{ duration: 2000 }" href="#event">Sobre</a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" v-smooth-scroll="{ duration: 2000 }" href="#where">Local</a>
+          </li>
+          <!-- <li class="nav-item active">
+            <a class="nav-link">Palestrantes</a>
+          </li> -->
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+          <button class="btn my-2 my-sm-0 btn-ticket" type="submit" @click.prevent="buyTicket">Comprar ingresso</button>
+        </form>
+      </div>
+    </div>
+  </nav>
 </template>
 
 <script>
@@ -28,18 +35,30 @@
 </script>
 
 <style lang="stylus" scoped>
-  $border = solid 2px #fff
-  $padding = 0 4rem
-  $margin = 1rem
   .menu-nav
+    background #000
     .nav
       a
-        color: #fff
-        text-shadow 0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23)
+        color #fff
+        &:hover
+          color #ffc300
 
-  @media (max-width: 767px) {
-    .menu-nav {
+  @media (max-width: 767px)
+    .menu-nav
       display none
-    }
-  }
+
+.btn-ticket
+  margin 6px
+  padding 10px 16px
+  border 2px solid #ffc300
+  border-radius 2px
+  text-transform uppercase
+  cursor pointer
+  font-weight 100
+  font-size .8em
+  color #000
+  background #ffc300
+  &:hover
+    background #000
+    color #ffc300
 </style>
