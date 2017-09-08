3dc648aadca85a4632ea2f2e11c22d5b80c7ff63
Jader Gomes <jjadergomes@gmail.com>
2017-09-08 13:01:45 -0300

    Init new component

---
+++ src/components/Pages/Contacts.vue
@@ -0,0 +1,23 @@
+<template>
+  <section class="contacts">
+  	<div class="container">
+  		<div class="row">
+  			<div class="col">
+  				<h1 class="header-title">Contato</h1>
+  			</div>
+  		</div>
+  	</div>
+  </section>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="stylus" scoped>
+.contacts
+	min-height 40vh
+	background #22292c
+	padding 20px 0 20px
+</style>
