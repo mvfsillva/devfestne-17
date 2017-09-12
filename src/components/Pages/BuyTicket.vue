97051efc4aa9d7d9f97ef496592b78c1e2e78840
Marcus SIlva <mvfsillva@gmail.com>
2017-09-12 01:23:05 -0300

    Renamed hastag defne2k17 to dfne2k17

--- src/components/Pages/BuyTicket.vue
+++ src/components/Pages/BuyTicket.vue
@@ -1,29 +1,29 @@
 <template>
-    <section class="ticket" id="ticket">
-        <div class="container">
-            <div class="row">
-                <div class="col">
-                    <h1 class="ticket-title">#vemprabahia #defne2k17</h1>
-                </div>
-            </div>
-            <div class="row justify-content-center">
-                <a href="https://loja.tampa.works/produto/devfest-nordeste-2017/" target="_blank" class="link-ticket">
-                    <img :src="Ticket" alt="Comprar ingresso">
-                </a>
-            </div>
+  <section class="ticket" id="ticket">
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <h1 class="ticket-title">#vemprabahia #dfne2k17</h1>
         </div>
-    </section>
+      </div>
+      <div class="row justify-content-center">
+        <a href="https://loja.tampa.works/produto/devfest-nordeste-2017/" target="_blank" class="link-ticket">
+          <img :src="Ticket" alt="Comprar ingresso">
+        </a>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
-    import Ticket from 'assets/img/ticket.png'
-    export default {
-        data() {
-            return {
-                Ticket
-            }
-        }
+import Ticket from 'assets/img/ticket.png'
+export default {
+  data() {
+    return {
+      Ticket
     }
+  }
+}
 </script>
 
 <style lang="stylus" scoped>
