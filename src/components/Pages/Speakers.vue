2eaaacfe2e76d3e2e52ff21bee88401c4ea66d5e
Jader Gomes <jjadergomes@gmail.com>
2017-09-29 08:51:11 -0300

    Add speakers using cards components

--- src/components/Pages/Speakers.vue
+++ src/components/Pages/Speakers.vue
@@ -1,58 +1,68 @@
 <template>
 <section id="speakers" class="speakers text-center">
 	<div class="container">
-		<div class="row">
-			<div class="col-md-12 col-md-offset-1">
-				<h2>
-	         <span class="block-title"> Palestrantes </span>
-	      </h2>
-			</div>
-		</div>
-		<div class="row speaker-grid">
-			<speaker-card speakerName="Yan Magalhães" :speakerPhoto="Yan" color="yellow" talkName="#UseThePlatform - Levando suas aplicações ao próximo nível com polymer" />
-			<speaker-card speakerName="Serena Fernandes" :speakerPhoto="Serena" color="red" talkName="Test Driven Development" />
-			<speaker-card speakerName="Matheus Marsiglio" :speakerPhoto="Matheus" color="red" talkName="Imutabilidade em objetos com Javascript" />
+		<block-title title="Palestrantes" color="white" />
+		<div class="speaker-grid row justify-content-center">
+			<card-speaker speakerName="Yan Magalhães" :speakerPhoto="Yan" color="yellow" talkName="#UseThePlatform - Levando suas aplicações ao próximo nível com polymer" />
+			<card-speaker speakerName="Serena Fernandes" :speakerPhoto="Serena" color="red" talkName="Test Driven Development" />
+			<card-speaker speakerName="Matheus Marsiglio" :speakerPhoto="Matheus" color="green" talkName="Imutabilidade em objetos com Javascript" />
+			<card-speaker speakerName="Maciek Ruckgaber" :speakerPhoto="Maciek" color="blue" talkName="IoT with raspberry Pi and MATRIX Creator" />
+			<card-speaker speakerName="Fernando Daciuk" :speakerPhoto="Daciuk" color="green" talkName="The secret of functions" />
+			<card-speaker speakerName="Alda Rocha" :speakerPhoto="Alda" color="yellow" talkName="Raio-x de UX" />
+			<card-speaker speakerName="José Manzoli" :speakerPhoto="Manzoli" color="blue" talkName="Troubleshooting - Do caos à rápida solução de problemas utilizando ferramentas como: Splunk, NewRelic e Perf4j" />
+			<card-speaker speakerName="Marina Limeira" :speakerPhoto="Marina" color="red" talkName="Crie sua própria realidade utilizando A-frame" />
+			<card-speaker speakerName="Geisa Santos" :speakerPhoto="Geisa" color="yellow" talkName="Hackeando além do Wi-Fi" />
+			<card-speaker speakerName="Evelyn Mendes" :speakerPhoto="Evelyn" color="red" talkName="Angular 4 + Firebase, realtime não precisa ser complicado" />
+			<card-speaker speakerName="Andrews Medina" :speakerPhoto="Medina" color="green" talkName="Escrevendo uma aplicação realtime para milhares de usuários simultâneos" />
+			<card-speaker speakerName="Thaise Dantas" :speakerPhoto="Thaise" color="blue" talkName="Testes! E aí de quem é essa responsabilidade?" />
 		</div>
 	</div>
 </section>
 </template>
 
 <script>
-import SpeakerCard from '../Template/SpeakerCard'
+import BlockTitle from '../Template/BlockTitle'
+import CardSpeaker from '../Template/CardSpeaker'
 import Yan from 'assets/img/speakers/Yan.png'
 import Serena from 'assets/img/speakers/Serena.png'
 import Matheus from 'assets/img/speakers/Matheus.png'
+import Maciek from 'assets/img/speakers/Maciek.png'
+import Daciuk from 'assets/img/speakers/Daciuk.png'
+import Alda from 'assets/img/speakers/Alda.png'
+import Manzoli from 'assets/img/speakers/Manzoli.png'
+import Marina from 'assets/img/speakers/Marina.png'
+import Geisa from 'assets/img/speakers/Geisa.png'
+import Evelyn from 'assets/img/speakers/Evelyn.png'
+import Medina from 'assets/img/speakers/Medina.png'
+import Thaise from 'assets/img/speakers/Thaise.png'
 
 export default {
 	data() {
 		return {
 			Yan,
 			Serena,
-			Matheus
+			Matheus,
+			Maciek,
+			Daciuk,
+			Alda,
+			Manzoli,
+			Marina,
+			Geisa,
+			Evelyn,
+			Medina,
+			Thaise
 		}
 	},
-	components: { SpeakerCard }
+	components: { BlockTitle, CardSpeaker }
 }
 </script>
 
 <style lang="stylus" scoped>
 .speakers
-	height 100vh
-	background-color #323131
-
-.block-title
-	text-transform uppercase
-	padding 5px
-	display inline-block
-	line-height 48px
-	font-size 28px
-	font-family 'Exo 2', sans-serif
-	color #fff
+	height auto
+	background-color #000
+	padding-bottom 2rem
 
 .speaker-grid
-	display -webkit-box
-	display -ms-flexbox
-	display flex
-	-ms-flex-wrap wrap
-	flex-wrap wrap
+	margin-top 2rem
 </style>
