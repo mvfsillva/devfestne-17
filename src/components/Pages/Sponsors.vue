1c6266b50ab88415c4985ad8fa43194a4106c2e9
Jader Gomes <jjadergomes@gmail.com>
2017-10-05 22:21:49 -0300

    Add component to show image, adjust grid

--- src/components/Pages/Sponsors.vue
+++ src/components/Pages/Sponsors.vue
@@ -1,45 +1,52 @@
 <template>
   <section class="sponsors">
-    <div class="container">
-      <div class="row">
-        <div class="col">
-          <h1 class="header-title sponsors-title">Patrocínio</h1>
-          <div class="praise-icon"><img :src="Praise" class="praise-icon" alt="praise icon"></div>
-        </div>
-      </div>
+    <div class="container text-center">
+      <block-title title="Patrocínio" color="black" />
       <div class="row">
         <div class="col">
           <p class="header-text">
-            Quem torna esse evento possível são as empresas que apoiam, investem e acreditam nesse projeto.
-            <br> Faça parte dessa iniciativa,
-            <a class="anchor" href="https://goo.gl/wMnezP" target="_blank">Seja um patrocinador!</a>
+            Agradecemos as empresas que apoiam, investem e acreditam nesse projeto.
+            <br> Faça parte,
+            <a class="anchor" href="https://goo.gl/wMnezP" target="_blank">seja um patrocinador!</a>
           </p>
         </div>
       </div>
-      <div class="row sponsors-logo">
-        <div class="col col-4">
+      <!-- sponsors: Block 1 -->
+      <div class="row align-items-center">
+        <div class="col-sm sponsors-column">
           <a href="https://developers.google.com/" target="_black">
-            <sponsor-partner-logo :logoImage="GoogleDevs" />
+            <sponsor-partner-logo :logoImage="Google" class="google-img" />
           </a>
         </div>
       </div>
-      <div class="row sponsors-logo">
-        <div class="col col-4">
+      <!-- sponsors: Block 2 -->
+      <div class="row align-items-center">
+        <div class="col-sm sponsors-column">
           <a href="http://www.senaicimatec.com.br/" target="_black">
             <sponsor-partner-logo :logoImage="Cimatec" />
           </a>
         </div>
-      </div>
-      <div class="row sponsors-logo">
-        <div class="col col-12 space-around">
-          <a href="https://www.umbler.com/" target="_black">
-            <sponsor-partner-logo :logoImage="Umbler" />
-          </a>
+        <div class="col-sm sponsors-column">
           <a href="http://tampa.works/" target="_black">
             <sponsor-partner-logo :logoImage="Tampa" />
           </a>
-          <a href="https://www.jusbrasil.com.br/" target="_black">
+        </div>
+        <div class="col-sm sponsors-column">
+          <a href="https://www.umbler.com/" target="_black">
+            <sponsor-partner-logo :logoImage="Umbler" />
+          </a>
+        </div>
+      </div>
+      <!-- sponsors: Block 3 -->
+      <div class="row align-items-center">
+        <div class="col-sm sponsors-column">
+          <a href="https://www.jusbrasil.com.br/home" target="_black">
             <sponsor-partner-logo :logoImage="Jus" />
+          </a>
+        </div>
+        <div class="col-sm sponsors-column">
+          <a href="http://www.wexinc.com.br/" target="_black">
+            <sponsor-partner-logo :logoImage="Wex" />
           </a>
         </div>
       </div>
@@ -48,52 +55,40 @@
 </template>
 
 <script>
-import Praise from 'assets/img/praise.png'
+import BlockTitle from '../Template/BlockTitle'
 import SponsorPartnerLogo from '../Template/SponsorPartnerLogo'
-import GoogleDevs from 'assets/img/logos/google_devs.png'
+import Google from 'assets/img/logos/google.png'
 import Cimatec from 'assets/img/logos/cimatec.png'
-import Umbler from 'assets/img/logos/umbler.png'
 import Tampa from 'assets/img/logos/tampa.png'
+import Umbler from 'assets/img/logos/umbler.png'
 import Jus from 'assets/img/logos/jusbrasil.png'
+import Wex from 'assets/img/logos/wex.png'
 
 export default {
   data() {
     return {
-      Praise,
-      GoogleDevs,
+      Google,
       Cimatec,
-      Umbler,
       Tampa,
-      Jus
+      Umbler,
+      Jus,
+      Wex
     }
   },
-  components: { SponsorPartnerLogo }
+  components: { BlockTitle, SponsorPartnerLogo }
 }
 </script>
 
 <style lang="stylus" scoped>
 .sponsors
-  padding 40px 0 40px 0
-  min-height 50vh
+  padding 40px 0 20px 0
+  min-height 40vh
   background #fff
 
-.sponsors-title
-  color #000
+.sponsors-column
+  padding 40px
 
-.praise-icon
-  display inline-block
-  width 44px
-  height auto
-  margin-left 10px
-  font-size 3.8rem
-  line-height 1
-
-.sponsors-logo
-  margin-top 3rem
-  display flex
-  justify-content center
-
-.space-around
-  display flex
-  justify-content space-around
+@media screen and (max-width: 400px) 
+  .google-img
+    width 260px
 </style>
