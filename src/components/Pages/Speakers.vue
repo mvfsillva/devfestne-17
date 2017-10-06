b2995923ac74ea0cc9ec101b98762549b7f64d53
Jader Gomes <jjadergomes@gmail.com>
2017-10-06 01:53:56 -0300

    Add more three speakers in the list

--- src/components/Pages/Speakers.vue
+++ src/components/Pages/Speakers.vue
@@ -1,24 +1,27 @@
 <template>
-<section id="speakers" class="speakers text-center">
-	<div class="container">
-		<block-title title="Palestrantes" color="white" />
-		<h5 class="text-center info"> Aguarde, em breve divulgaremos a lista completa. </h5>
-		<div class="speaker-grid row justify-content-center">
-			<card-speaker speakerName="Yan Magalhães" :speakerPhoto="Yan" color="yellow" talkName="#UseThePlatform - Levando suas aplicações ao próximo nível com polymer" />
-			<card-speaker speakerName="Serena Fernandes" :speakerPhoto="Serena" color="red" talkName="Test Driven Development" />
-			<card-speaker speakerName="Matheus Marsiglio" :speakerPhoto="Matheus" color="green" talkName="Imutabilidade em objetos com Javascript" />
-			<card-speaker speakerName="Maciek Ruckgaber" :speakerPhoto="Maciek" color="blue" talkName="IoT with raspberry Pi and MATRIX Creator" />
-			<card-speaker speakerName="Fernando Daciuk" :speakerPhoto="Daciuk" color="green" talkName="The secret of functions" />
-			<card-speaker speakerName="Alda Rocha" :speakerPhoto="Alda" color="yellow" talkName="Raio-x de UX" />
-			<card-speaker speakerName="José Manzoli" :speakerPhoto="Manzoli" color="blue" talkName="Troubleshooting - Do caos à rápida solução de problemas utilizando ferramentas como: Splunk, NewRelic e Perf4j" />
-			<card-speaker speakerName="Marina Limeira" :speakerPhoto="Marina" color="red" talkName="Crie sua própria realidade utilizando A-frame" />
-			<card-speaker speakerName="Geisa Santos" :speakerPhoto="Geisa" color="yellow" talkName="Hackeando além do Wi-Fi" />
-			<card-speaker speakerName="Evelyn Mendes" :speakerPhoto="Evelyn" color="red" talkName="Angular 4 + Firebase, realtime não precisa ser complicado" />
-			<card-speaker speakerName="Andrews Medina" :speakerPhoto="Medina" color="green" talkName="Escrevendo uma aplicação realtime para milhares de usuários simultâneos" />
-			<card-speaker speakerName="Thaise Dantas" :speakerPhoto="Thaise" color="blue" talkName="Testes! E aí de quem é essa responsabilidade?" />
+	<section id="speakers" class="speakers text-center">
+		<div class="container">
+			<block-title title="Palestrantes" color="white" />
+			<h6 class="text-center info"> Aguarde, em breve divulgaremos a lista completa. </h6>
+			<div class="speaker-grid row justify-content-center">
+				<card-speaker speakerName="Yan Magalhães" :speakerPhoto="Yan" color="yellow" talkName="#UseThePlatform - Levando suas aplicações ao próximo nível com polymer" />
+				<card-speaker speakerName="Serena Fernandes" :speakerPhoto="Serena" color="red" talkName="Test Driven Development" />
+				<card-speaker speakerName="Matheus Marsiglio" :speakerPhoto="Matheus" color="green" talkName="Imutabilidade em objetos com Javascript" />
+				<card-speaker speakerName="Maciek Ruckgaber" :speakerPhoto="Maciek" color="blue" talkName="IoT with raspberry Pi and MATRIX Creator" />
+				<card-speaker speakerName="Fernando Daciuk" :speakerPhoto="Daciuk" color="green" talkName="The secret of functions" />
+				<card-speaker speakerName="Alda Rocha" :speakerPhoto="Alda" color="yellow" talkName="Raio-x de UX" />
+				<card-speaker speakerName="José Manzoli" :speakerPhoto="Manzoli" color="blue" talkName="Troubleshooting - Do caos à rápida solução de problemas utilizando ferramentas como: Splunk, NewRelic e Perf4j" />
+				<card-speaker speakerName="Marina Limeira" :speakerPhoto="Marina" color="red" talkName="Crie sua própria realidade utilizando A-frame" />
+				<card-speaker speakerName="Geisa Santos" :speakerPhoto="Geisa" color="yellow" talkName="Hackeando além do Wi-Fi" />
+				<card-speaker speakerName="Evelyn Mendes" :speakerPhoto="Evelyn" color="red" talkName="Angular 4 + Firebase, realtime não precisa ser complicado" />
+				<card-speaker speakerName="Andrews Medina" :speakerPhoto="Medina" color="green" talkName="Escrevendo uma aplicação realtime para milhares de usuários simultâneos" />
+				<card-speaker speakerName="Thaise Dantas" :speakerPhoto="Thaise" color="blue" talkName="Testes! E aí de quem é essa responsabilidade?" />
+				<card-speaker speakerName="Maria Clara" :speakerPhoto="Maria" color="green" talkName="O guia do mochileiro para segurança client-side" />
+				<card-speaker speakerName="Thiago Almeida" :speakerPhoto="Thiago" color="yellow" talkName="Acessibilidade web - como estamos pensando?" />
+				<card-speaker speakerName="Bruno Pulis" :speakerPhoto="Pulis" color="blue" talkName="Componentes: o lado sombrio que não te contam!" />
+			</div>
 		</div>
-	</div>
-</section>
+	</section>
 </template>
 
 <script>
@@ -36,6 +39,9 @@ import Geisa from 'assets/img/speakers/Geisa.png'
 import Evelyn from 'assets/img/speakers/Evelyn.png'
 import Medina from 'assets/img/speakers/Medina.png'
 import Thaise from 'assets/img/speakers/Thaise.png'
+import Maria from 'assets/img/speakers/Maria.png'
+import Thiago from 'assets/img/speakers/Thiago.png'
+import Pulis from 'assets/img/speakers/Pulis.jpg'
 
 export default {
 	data() {
@@ -51,7 +57,10 @@ export default {
 			Geisa,
 			Evelyn,
 			Medina,
-			Thaise
+			Thaise,
+			Maria,
+			Thiago,
+			Pulis
 		}
 	},
 	components: {
