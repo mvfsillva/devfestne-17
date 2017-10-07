e1166273bb7b928f11bed27b0ed8a59c002b51f8
Jader Gomes <jjadergomes@gmail.com>
2017-10-07 15:06:36 -0300

    Remove background image, refactory text and adjust responsive

--- src/components/Pages/ViewMap.vue
+++ src/components/Pages/ViewMap.vue
@@ -1,63 +1,75 @@
 <template>
-  <div id="where" class="map">
+  <section id="where" class="map text-center">
     <div class="container">
-      <div class="row">
-        <div class="col">
-          <h1 class="header-title">Localização</h1>
-          <h4 class="cimatec">Centro Universitário SENAI CIMATEC</h4>
-          <p class="location">
-            Av. Orlando Gomes, 1845 - Piatã, Salvador - BA, 41650-010
-          </p>
+      <block-title title="Local" color="white" />
+      <div class="row justify-center-content">
+        <div class="col-md-12">
+          <div class="address">
+            <h4 class="cimatec"> Centro Universitário SENAI CIMATEC </h4>
+            <p> Av. Orlando Gomes, 1845 - Piatã, Salvador - BA, 41650-010 </p>
+          </div>
+          <div id="map" class="map-pointer"></div>
         </div>
       </div>
-      <div id="map" class="view-map"></div>
     </div>
-  </div>
+  </section>
 </template>
 
 <script>
+import BlockTitle from '../Template/BlockTitle'
 import mapsStyle from 'assets/json/maps-style.json'
 
 export default {
-  mounted () {
+  mounted() {
     this.createMap()
   },
   methods: {
-    createMap: function () {
+    createMap: function() {
       let map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: -12.9381642, lng: -38.3861483},
+        center: { lat: -12.9381642, lng: -38.3861483 },
         zoom: 15,
         styles: mapsStyle
       })
 
       this.marker = new google.maps.Marker({
-        position: {lat: -12.9388791, lng: -38.3864129},
+        position: { lat: -12.9388791, lng: -38.3864129 },
         map: map
       })
     }
-  }
+  },
+  components: { BlockTitle }
 }
 </script>
 
 <style lang="stylus" scoped>
-  .map
-    min-height 100vh
-    padding 80px 0 20px 0
+.map
+  min-height 60vh
+  padding 20px
 
-    .cimatec
-      color #ffc300
-      line-height 1.5
+.address
+  margin 10px
+  font-family 'Exo 2', sans-serif
+  > .cimatec
+      font-weight 800
+      font-size 1rem
       text-transform uppercase
-      font-weight 100
-      font-size 16px
-      padding-top 1rem
+      color #00a158
+  > p 
+      font-size 1rem
+      font-weight 400
+      line-height 1.5
+      color #00a158
 
-    .location
-      color #ffc300
-      font-family 'Raleway', sans-serif
+.map-pointer
+  width 100%
+  height 60vh
+  text-align center
 
-    .view-map
-      width 100%
-      height 60vh
-      margin-top 1rem
+@media screen and (max-width: 600px) 
+  .map-pointer
+    height 40vh
+
+@media screen and (max-width: 400px) 
+  .map-pointer
+    height 30vh
 </style>
