e5894e3bc80d7378aa84d68333e79f41ff92ae34
Jader Gomes <jjadergomes@gmail.com>
2017-10-18 02:12:54 -0300

    Refactory map section

--- src/components/Pages/ViewMap.vue
+++ src/components/Pages/ViewMap.vue
@@ -1,15 +1,13 @@
 <template>
-  <section id="where" class="map text-center">
+  <section class="map text-center">
     <div class="container">
       <block-title title="Local" color="white" />
-      <div class="row justify-center-content">
+      <div class="row">
         <div class="col-md-12">
-          <div class="address">
-            <h4 class="cimatec"> Centro Universitário SENAI CIMATEC </h4>
-            <p> Av. Orlando Gomes, 1845 - Piatã, Salvador - BA, 41650-010 </p>
-          </div>
+          <h4 class="cimatec"> Centro Universitário SENAI CIMATEC </h4>
+          <p> Av. Orlando Gomes, 1845 - Piatã, Salvador - BA, 41650-010 </p>
+          <button class="btn my-2 my-sm-0 btn-map" type="submit" @click.prevent="viewMap">Ver no Mapa</button>
         </div>
-        <div id="map" class="map-pointer"></div>
       </div>
     </div>
   </section>
@@ -20,21 +18,9 @@ import BlockTitle from '../Template/BlockTitle'
 import mapsStyle from 'assets/json/maps-style.json'
 
 export default {
-  mounted() {
-    this.createMap()
-  },
   methods: {
-    createMap: function() {
-      let map = new google.maps.Map(document.getElementById('map'), {
-        center: { lat: -12.9381642, lng: -38.3861483 },
-        zoom: 15,
-        styles: mapsStyle
-      })
-
-      this.marker = new google.maps.Marker({
-        position: { lat: -12.9388791, lng: -38.3864129 },
-        map: map
-      })
+      viewMap() {
+        window.open('http://bit.ly/2yvuMHU', '_blank')
     }
   },
   components: { BlockTitle }
@@ -44,32 +30,46 @@ export default {
 <style lang="stylus" scoped>
 .map
   min-height 60vh
-  padding 20px
-
-.address
-  margin 10px
-  font-family 'Roboto', sans-serif
-  > .cimatec
-      font-weight 800
-      font-size 1rem
+  padding 60px
+  background-image url(../../assets/img/others/senai.png)
+  background-size cover
+  
+ .cimatec
+      font-weight 600
+      font-size 1.4rem
+      font-family 'Roboto', sans-serif
       text-transform uppercase
-      color #4887f7
-  > p 
+      margin-top 4rem
+      color #fff
+p 
       font-size 1rem
       font-weight 400
       line-height 1.5
-      color #4887f7
-
-.map-pointer
-  width 100%
-  height 60vh
-  text-align center
+      margin-top 1.6rem
+      margin-bottom 3rem
+      color #fff
 
 @media screen and (max-width: 600px) 
-  .map-pointer
+  .map
     height 40vh
 
 @media screen and (max-width: 400px) 
-  .map-pointer
+  .map
     height 30vh
+
+.btn-map
+  margin 8px
+  padding 12px 16px
+  border 2px solid #4887f7
+  border-radius 2px
+  text-transform uppercase
+  cursor pointer
+  font-size 1em
+  font-family 'Roboto', sans-serif
+  font-weight 200
+  color #fff
+  background #4887f7
+  &:hover
+    background #000
+    color #4887f7    
 </style>
