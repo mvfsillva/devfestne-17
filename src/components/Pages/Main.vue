8acf4c2bb8e53d6333810e519600e0fb2e4958d4
Jader Gomes <jjadergomes@gmail.com>
2017-10-18 03:15:32 -0300

    Add photo and create component to presenter

--- src/components/Pages/Main.vue
+++ src/components/Pages/Main.vue
@@ -1,6 +1,5 @@
 <template>
   <main>
-    <!-- <menu-nav/> -->
     <main-header/>
     <event-info/>
     <view-map/>
@@ -9,34 +8,36 @@
     <schedule/>
     <sponsors/>
     <partners/>
+    <presenter/>
     <footer-page/>
   </main>
 </template>
 
 <script>
-  import MenuNav from '../Template/MenuNav'
   import MainHeader from './MainHeader'
   import EventInfo from './EventInfo'
   import ViewMap from './ViewMap'
   import Speakers from './Speakers'
   import Ticket from './Ticket'
+  import Schedule from './Schedule'
   import Sponsors from './Sponsors'
   import Partners from './Partners'
+  import Presenter from './Presenter'
   import FooterPage from './Footer'
-  import Schedule from './Schedule'
+  
 
   export default {
     components: {
-      MenuNav,
       MainHeader,
       EventInfo,
       ViewMap,
       Speakers,
       Ticket,
+      Schedule,
       Sponsors,
       Partners,
-      FooterPage,
-      Schedule
+      Presenter,
+      FooterPage
     }
   }
 </script>
---
+++ src/components/Pages/Presenter.vue
@@ -0,0 +1,28 @@
+<template>
+  <section class="presenter">
+      <div class="container">
+          <div class="row justify-content-center">
+              <card-speaker speakerName="Rafael Verger" :speakerPhoto="Rafa" color="yellow" workAt="#verger" cityName="Apresentador"/>
+          </div>
+      </div>
+  </section>
+</template>
+
+<script>
+import CardSpeaker from '../Template/CardSpeaker'
+import Rafa from 'assets/img/speakers/rafa.png'
+
+export default {
+  data() {
+      return {
+          Rafa
+      }
+  },
+  components: {CardSpeaker}
+}
+</script>
+
+<style lang="stylus" scoped>
+.presenter
+    background #000
+</style>
