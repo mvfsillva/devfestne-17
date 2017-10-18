d2774d880ad1fefc5310ec519d4898b32ca53207
Jader Gomes <jjadergomes@gmail.com>
2017-10-18 00:45:29 -0300

    Include talk name and hours

--- src/components/Pages/Schedule.vue
+++ src/components/Pages/Schedule.vue
@@ -1,13 +1,334 @@
 <template>
-    <section class="schedule"></section>
+    <section class="schedule">
+        <div class="container text-center">
+            <block-title title="Programação" color="black"/>
+        </div>
+        <div class="container">
+            <ul class="list-group">
+                <li class=" list-group-item bottom-line">
+                    <div class="row">
+                        <div class="col-2">
+                            <div class="hours"> 8:00 am </div>
+                        </div>
+                        <div class="col-10">
+                             <span class="tab-content"> Credenciamento </span>                                                     
+                        </div>
+                    </div>
+                </li>
+                <li class="list-group-item bottom-line">
+                    <div class="row">
+                        <div class="col-2">
+                            <div class="hours"> 9:00 am </div>
+                        </div>
+                        <div class="col-10">
+                             <span class="tab-content"> Abertura do Devfest nordeste </span>                                                     
+                        </div>
+                    </div>
+                </li>
+                <li class="list-group-item bottom-line">
+                    <div class="row">
+                        <div class="col-2">
+                            <div class="hours"> 9:15 am </div>
+                            <div class="blue-label"> PALCO 1 </div>                           
+                        </div>
+                        <div class="col-10">
+                             <span class="tab-content"> Hackeando além do Wi-Fi </span>   
+                             <p class="speaker-name"> Geisa Santos </p>                                             
+                        </div>
+                    </div>
+                </li>
+                <li class="list-group-item bottom-line">
+                    <div class="row">
+                        <div class="col-2">
+                            <div class="hours"> 9:15 am </div>
+                            <div class="green-label"> PALCO 2 </div>
+                        </div>
+                        <div class="col-10">
+                             <span class="tab-content"> Realidade virtual e o Futuro </span>   
+                             <p class="speaker-name"> Alano Acioli </p>                                                  
+                        </div>
+                    </div>
+                </li>
+                <li class="list-group-item bottom-line">
+                    <div class="row">
+                        <div class="col-2">
+                            <div class="hours"> 9:45 am </div>
+                            <div class="blue-label"> PALCO 1 </div>                           
+                        </div>
+                        <div class="col-10">
+                             <span class="tab-content"> #UseThePlatform - Levando suas aplicações ao próximo nível com Polymer </span>   
+                             <p class="speaker-name"> Yan Magalhães </p>                                             
+                        </div>
+                    </div>
+                </li>
+                <li class="list-group-item bottom-line">
+                    <div class="row">
+                        <div class="col-2">
+                            <div class="hours"> 9:45 am </div>
+                            <div class="green-label"> PALCO 2 </div>
+                        </div>
+                        <div class="col-10">
+                             <span class="tab-content"> Testes! E aí de quem é essa responsabilidade? </span>   
+                             <p class="speaker-name"> Thaise Dantas </p>                                                  
+                        </div>
+                    </div>
+                </li>
+                <li class="list-group-item bottom-line">
+                    <div class="row">
+                        <div class="col-2">
+                            <div class="hours"> 10:15 am </div>
+                            <div class="blue-label"> PALCO 1 </div>                           
+                        </div>
+                        <div class="col-10">
+                             <span class="tab-content"> Test Driven Development </span>   
+                             <p class="speaker-name"> Serena Fernandes </p>                                             
+                        </div>
+                    </div>
+                </li>
+                <li class="list-group-item bottom-line">
+                    <div class="row">
+                        <div class="col-2">
+                            <div class="hours"> 10:15 am </div>
+                            <div class="green-label"> PALCO 2 </div>
+                        </div>
+                        <div class="col-10">
+                             <span class="tab-content"> Componentes: o lado sombrio que não te contaram! </span>   
+                             <p class="speaker-name"> Bruno Pulis </p>                                                  
+                        </div>
+                    </div>
+                </li>
+                <li class="list-group-item bottom-line">
+                    <div class="row">
+                        <div class="col-2">
+                            <div class="hours"> 10:45 am </div>
+                            <div class="blue-label"> PALCO 1 </div>                           
+                        </div>
+                        <div class="col-10">
+                             <span class="tab-content"> Escrevendo uma aplicação realtime para milhares de usuários simultâneos </span>   
+                             <p class="speaker-name"> Andrews Medina </p>                                             
+                        </div>
+                    </div>
+                </li>
+                <li class="list-group-item bottom-line">
+                    <div class="row">
+                        <div class="col-2">
+                            <div class="hours"> 10:45 am </div>
+                            <div class="green-label"> PALCO 2 </div>
+                        </div>
+                        <div class="col-10">
+                             <span class="tab-content"> Projetando aplicativos para lidar com conexões lentas. </span>   
+                             <p class="speaker-name"> Italo Marcel </p>                                                  
+                        </div>
+                    </div>
+                </li>
+                <li class="list-group-item bottom-line">
+                    <div class="row">
+                        <div class="col-2">
+                            <div class="hours"> 11:15 am </div>
+                            <div class="blue-label"> PALCO 1 </div>                           
+                        </div>
+                        <div class="col-10">
+                             <span class="tab-content"> Troubleshooting-Do caos à rápida solução de problemas utilizando ferramentas como: Splunk, NewRelic e Perf4J. </span>   
+                             <p class="speaker-name"> José Luiz Manzoli </p>                                             
+                        </div>
+                    </div>
+                </li>
+                <li class="list-group-item bottom-line">
+                    <div class="row">
+                        <div class="col-2">
+                            <div class="hours"> 11:15 am </div>
+                            <div class="green-label"> PALCO 2 </div>
+                        </div>
+                        <div class="col-10">
+                             <span class="tab-content"> Inovação com ferramentas Google </span>   
+                             <p class="speaker-name"> Marla Dore </p>                                                  
+                        </div>
+                    </div>
+                </li>
+                <li class="list-group-item bottom-line">
+                    <div class="row">
+                        <div class="col-2">
+                            <div class="hours"> 11:50 am </div>
+                        </div>
+                        <div class="col-10">
+                             <span class="tab-content"> Almoço </span>   
+                             <p class="speaker-name"> Restaurantes no local </p>                                                  
+                        </div>
+                    </div>
+                </li>
+                <li class="list-group-item bottom-line">
+                    <div class="row">
+                        <div class="col-2">
+                            <div class="hours"> 13:40 pm </div>
+                        </div>
+                        <div class="col-10">
+                             <span class="tab-content"> Crie sua própria realidade utilizando A-frame </span>   
+                             <p class="speaker-name"> Marina Limeira </p>                                                  
+                        </div>
+                    </div>
+                </li>
+                <li class="list-group-item bottom-line">
+                    <div class="row">
+                        <div class="col-2">
+                            <div class="hours"> 14:10 pm </div>
+                        </div>
+                        <div class="col-10">
+                             <span class="tab-content"> Imutabilidade em objetos com Javascript </span>   
+                             <p class="speaker-name"> Matheus Marsiglio </p>                                                  
+                        </div>
+                    </div>
+                </li>
+                <li class="list-group-item bottom-line">
+                    <div class="row">
+                        <div class="col-2">
+                            <div class="hours"> 14:40 pm </div>
+                        </div>
+                        <div class="col-10">
+                             <span class="tab-content"> Raio-X de UX </span>   
+                             <p class="speaker-name"> Alda Rocha </p>                                                  
+                        </div>
+                    </div>
+                </li>
+                <li class="list-group-item bottom-line">
+                    <div class="row">
+                        <div class="col-2">
+                            <div class="hours"> 15:10 pm </div>
+                        </div>
+                        <div class="col-10">
+                             <span class="tab-content"> The secrets of functions </span>   
+                             <p class="speaker-name"> Fernando Daciuk </p>                                                  
+                        </div>
+                    </div>
+                </li>
+                <li class="list-group-item bottom-line">
+                    <div class="row">
+                        <div class="col-2">
+                            <div class="hours"> 15:40 pm </div>
+                        </div>
+                        <div class="col-10">
+                             <span class="tab-content"> Coffee Break </span>                                                  
+                        </div>
+                    </div>
+                </li>
+                <li class="list-group-item bottom-line">
+                    <div class="row">
+                        <div class="col-2">
+                            <div class="hours"> 16:10 pm </div>
+                        </div>
+                        <div class="col-10">
+                             <span class="tab-content"> Angular 4 + Firebase, realtime não precisa ser complicado </span>   
+                             <p class="speaker-name"> Evelyn Mendes </p>                                                  
+                        </div>
+                    </div>
+                </li>
+                <li class="list-group-item bottom-line">
+                    <div class="row">
+                        <div class="col-2">
+                            <div class="hours"> 16:40 pm </div>
+                        </div>
+                        <div class="col-10"> 
+                             <span class="tab-content"> IoT with Raspberry Pi and MATRIX Creator </span>   
+                             <p class="speaker-name"> Maciek Ruckgaber </p>                                                
+                        </div>
+                    </div>
+                </li>
+                <li class="list-group-item bottom-line">
+                    <div class="row">
+                        <div class="col-2">
+                            <div class="hours"> 17:10 pm </div>
+                        </div>
+                        <div class="col-10">
+                            <span class="tab-content"> Acessibilidade web - como estamos pensando? </span>   
+                            <p class="speaker-name"> Thiago Almeida </p>                                           
+                        </div>
+                    </div>
+                </li>
+                <li class="list-group-item bottom-line">
+                    <div class="row">
+                        <div class="col-2">
+                            <div class="hours"> 17:40 pm </div>
+                        </div>
+                        <div class="col-10">
+                             <span class="tab-content"> Sorteio de brindes </span>                                                  
+                        </div>
+                    </div>
+                </li>
+                <li class="list-group-item">
+                    <div class="row">
+                        <div class="col-2">
+                            <div class="hours"> 18:00 pm </div>
+                        </div>
+                        <div class="col-10">
+                             <span class="tab-content"> Encerramento </span>                                                  
+                        </div>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </section>
 </template>
 
 <script>
+import BlockTitle from '../Template/BlockTitle'
+
 export default {
-  
+    components: {BlockTitle}
 }
 </script>
 
 <style lang="stylus" scoped>
+.schedule
+    padding 40px 0 40px
+    background #fff
+    
+.list-group-item
+    border none
+    padding 26px 0px
+
+.bottom-line
+    border-bottom 2px solid rgba(0, 0, 0, 0.16) !important    
+
+.hours  
+    font-family 'Roboto'     
+
+.tab-content
+    font-family 'Raleway'
+    font-weight 600
+    font-size 17px
+    color #2b3137
+    text-transform uppercase    
+
+.speaker-name
+    font-family 'Roboto'
+    font-weight 400
+    font-size 14px
+    color #2b3137
+    text-transform uppercase
+
+.label
+    display inline-block
+    line-height 1
+    vertical-align baseline
+    margin 0 .1em
+    padding .4833em .733em
+    font-size 11px
+    color #fff 
+    text-transform none
+    font-weight 400
+    font-family 'Roboto'
+    border 0 solid transparent
+    border-radius 0px
+    -webkit-transition: background .1s ease
+    transition background .1s ease
+
+.blue-label
+    @extend .label
+    background-color #4887f7
+    border-color #4887f7
+   
+.green-label
+    @extend .label
+    background-color #00a158
+    border-color #00a158   
 
 </style>
